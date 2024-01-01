fcfa32992ea270c6f21318e15d03ab787e438fdc
DmitryShadrin <DShadrin@sportmaster.ru>
2024-01-01 19:00:53 +0300

    The transition to ORM has been implemented

---
+++ hw06-orm/src/main/java/ru/otus/hw/Hw06OrmApplication.java
@@ -0,0 +1,12 @@
+package ru.otus.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw06OrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw06OrmApplication.class, args);
+    }
+}
---
+++ hw06-orm/src/test/java/ru/otus/hw/repositories/BookRepositoryJpaTest.java
@@ -0,0 +1,158 @@
+package ru.otus.hw.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+import ru.otus.hw.models.Author;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.models.Genre;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Репозиторий на основе Jpa для работы с книгами ")
+@DataJpaTest
+@Import({BookRepositoryJpa.class})
+class BookRepositoryJpaTest {
+
+    @Autowired
+    private BookRepositoryJpa repository;
+
+    @Autowired
+    private TestEntityManager em;
+
+    private List<Author> dbAuthors;
+
+    private List<Genre> dbGenres;
+
+    private List<Book> dbBooks;
+
+    private Book newBook;
+
+    private Book changeBook;
+
+    @BeforeEach
+    void setUp() {
+        dbAuthors = getDbAuthors();
+        dbGenres = getDbGenres();
+        dbBooks = getDbBooks(dbAuthors, dbGenres);
+        newBook = getNewBook();
+        changeBook = getChangeBook();
+    }
+
+    @DisplayName("должен загружать книгу по id")
+    @ParameterizedTest
+    @MethodSource("getDbBooks")
+    void shouldReturnCorrectBookById(Book expectedBook) {
+        var actualBook = repository.findById(expectedBook.getId());
+        assertThat(actualBook).isPresent()
+                .get()
+                .usingRecursiveComparison()
+                .isEqualTo(expectedBook);
+    }
+
+    @DisplayName("должен загружать список всех книг")
+    @Test
+    void shouldReturnCorrectBooksList() {
+        var actualBooks = repository.findAll();
+        var expectedBooks = dbBooks;
+
+        assertThat(actualBooks)
+                .hasSize(3)
+                .usingRecursiveComparison()
+                .isEqualTo(expectedBooks);
+        actualBooks.forEach(System.out::println);
+    }
+
+    @DisplayName("должен сохранять новую книгу")
+    @Test
+    void shouldSaveNewBook() {
+        var expectedBook = newBook;
+        var returnedBook = repository.save(expectedBook);
+        assertThat(returnedBook).isNotNull()
+                .matches(book -> book.getId() > 0)
+                .usingRecursiveComparison().ignoringExpectedNullFields().isEqualTo(expectedBook);
+
+        assertThat(repository.findById(returnedBook.getId()))
+                .isPresent()
+                .get()
+                .isEqualTo(returnedBook);
+    }
+
+    @DisplayName("должен сохранять измененную книгу")
+    @Test
+    void shouldSaveUpdatedBook() {
+        var expectedBook = changeBook;
+        assertThat(repository.findById(expectedBook.getId()))
+                .isPresent()
+                .get()
+                .isNotEqualTo(expectedBook);
+
+        var returnedBook = repository.save(expectedBook);
+        assertThat(returnedBook).isNotNull()
+                .matches(book -> book.getId() > 0)
+                .usingRecursiveComparison().ignoringExpectedNullFields().isEqualTo(expectedBook);
+
+        assertThat(repository.findById(returnedBook.getId()))
+                .isPresent()
+                .get()
+                .isEqualTo(returnedBook);
+    }
+
+    @DisplayName("должен удалять книгу по id ")
+    @Test
+    void shouldDeleteBook() {
+        assertThat(repository.findById(1L)).isPresent();
+        repository.deleteById(1L);
+        assertThat(repository.findById(1L)).isEmpty();
+    }
+
+    private static List<Author> getDbAuthors() {
+        return IntStream.range(1, 4).boxed()
+                .map(id -> new Author(id, "Author_" + id))
+                .toList();
+    }
+
+    private static List<Genre> getDbGenres() {
+        return IntStream.range(1, 7).boxed()
+                .map(id -> new Genre(id, "Genre_" + id))
+                .toList();
+    }
+
+    private static List<Book> getDbBooks(List<Author> dbAuthors, List<Genre> dbGenres) {
+        return IntStream.range(1, 4).boxed()
+                .map(id -> new Book(id,
+                        "BookTitle_" + id,
+                        dbAuthors.get(id - 1),
+                        dbGenres.subList((id - 1) * 2, (id - 1) * 2 + 2)
+                ))
+                .toList();
+    }
+
+    private static List<Book> getDbBooks() {
+        var dbAuthors = getDbAuthors();
+        var dbGenres = getDbGenres();
+        return getDbBooks(dbAuthors, dbGenres);
+    }
+
+    private Book getNewBook() {
+        Author author = em.find(Author.class, 3L);
+        List<Genre> genres = List.of(em.find(Genre.class, 3L), em.find(Genre.class, 4L));
+        return new Book(0, "BookTitle_5", author, genres);
+    }
+
+
+    private Book getChangeBook() {
+        Author author = em.find(Author.class, 1L);
+        List<Genre> genres = List.of(em.find(Genre.class, 1L), em.find(Genre.class, 2L));
+        return new Book(1, "BookTitle_10500", author, genres);
+    }
+}
