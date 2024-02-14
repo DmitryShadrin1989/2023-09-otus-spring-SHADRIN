d3cabbe7e1d897e1a6c9707a9bd9e60856be7897
DmitryShadrin <DShadrin@sportmaster.ru>
2024-02-14 10:54:08 +0300

    Refactoring after review

--- hw08-data-mongo/src/main/java/ru/otus/hw/models/Comment.java
+++ hw08-data-mongo/src/main/java/ru/otus/hw/models/Comment.java
@@ -6,11 +6,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Document(collection = "comments")
 public class Comment {
 
     @Id
